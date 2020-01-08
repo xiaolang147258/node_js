be98e89537f2823c5c7236b571bf0bed4f756329
xiaolang <2443611475@qq.com>
2020-01-08 16:31:27 +0800

    132

--- act.js
+++ act.js
@@ -34,7 +34,7 @@ app.use( bodyParser.urlencoded({
 
 // 设置图片存储路径
 var storage = multer.diskStorage({
-  destination: function(req, file, cb) {cb(null, './public/img');},
+  destination: function(req, file, cb) {cb(null, './img');},
   filename: function(req, file, cb) {cb(null, `${Date.now()}-${file.originalname}`)}
 })
 
