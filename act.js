17a009edd550629cdd64920d65c876b2b480cb24
xiaolang <2443611475@qq.com>
2020-01-10 16:04:51 +0800

    Update act.js

--- act.js
+++ act.js
@@ -63,16 +63,27 @@ app.all('*',function (req, res, next) {
 const os = require('os');
 ///////////////////获取本机ip///////////////////////
 function getIPAdress() {
-    var interfaces = os.networkInterfaces();
-    for (var devName in interfaces) {
-        var iface = interfaces[devName];
-        for (var i = 0; i < iface.length; i++) {
-            var alias = iface[i];
-            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-                return alias.address;
+        const osType = os.type(); //系统类型
+        const netInfo = os.networkInterfaces(); //网络信息
+        let ip = '';
+        if (osType === 'Windows_NT') { 
+            for (let dev in netInfo) {
+            	//win7的网络信息中显示为本地连接，win10显示为以太网
+                if (dev === '本地连接' || dev === '以太网') {
+                    for (let j = 0; j < netInfo[dev].length; j++) {
+                        if (netInfo[dev][j].family === 'IPv4') {
+                            ip = netInfo[dev][j].address;
+                            break;
+                        }
+                    }
+                }
             }
+    
+        } else if (osType === 'Linux') {
+            ip = netInfo.eth0[0].address;
         }
-    }
+    
+        return ip;
 }
 const myHost = getIPAdress();
 
